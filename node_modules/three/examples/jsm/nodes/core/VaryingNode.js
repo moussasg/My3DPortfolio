bd3065c6f1571a6e9f79abc34cb8f2886cc890d4
moussasg <moussa@aria-groups.com>
2024-03-09 18:42:49 +0400

    Update .gitignore

--- node_modules/three/examples/jsm/nodes/core/VaryingNode.js
+++ node_modules/three/examples/jsm/nodes/core/VaryingNode.js
@@ -1,5 +1,6 @@
-import Node from './Node.js';
+import Node, { addNodeClass } from './Node.js';
 import { NodeShaderStage } from './constants.js';
+import { addNodeElement, nodeProxy } from '../shadernode/ShaderNode.js';
 
 class VaryingNode extends Node {
 
@@ -9,6 +10,14 @@ class VaryingNode extends Node {
 
 		this.node = node;
 		this.name = name;
+
+		this.isVaryingNode = true;
+
+	}
+
+	isGlobal() {
+
+		return true;
 
 	}
 
@@ -28,22 +37,18 @@ class VaryingNode extends Node {
 
 	generate( builder ) {
 
+		const { name, node } = this;
 		const type = this.getNodeType( builder );
-		const node = this.node;
-		const name = this.name;
 
-		const nodeVarying = builder.getVaryingFromNode( this, type );
+		const nodeVarying = builder.getVaryingFromNode( this, name, type );
 
-		if ( name !== null ) {
+		// this property can be used to check if the varying can be optimized for a var
+		nodeVarying.needsInterpolation || ( nodeVarying.needsInterpolation = ( builder.shaderStage === 'fragment' ) );
 
-			nodeVarying.name = name;
-
-		}
-
-		const propertyName = builder.getPropertyName( nodeVarying, NodeShaderStage.Vertex );
+		const propertyName = builder.getPropertyName( nodeVarying, NodeShaderStage.VERTEX );
 
 		// force node run in vertex stage
-		builder.flowNodeFromShaderStage( NodeShaderStage.Vertex, node, type, propertyName );
+		builder.flowNodeFromShaderStage( NodeShaderStage.VERTEX, node, type, propertyName );
 
 		return builder.getPropertyName( nodeVarying );
 
@@ -52,3 +57,9 @@ class VaryingNode extends Node {
 }
 
 export default VaryingNode;
+
+export const varying = nodeProxy( VaryingNode );
+
+addNodeElement( 'varying', varying );
+
+addNodeClass( 'VaryingNode', VaryingNode );
