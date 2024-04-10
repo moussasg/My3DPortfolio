88b74dde7467741f4065b9887bc7a627238da662
moussasg <moussa@aria-groups.com>
2024-04-10 16:54:11 +0400

    testf

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -24,7 +24,7 @@ const ServiceCard: React.FC<IServiceCard> = ({ index, title, icon }) => (
   >
     <div className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right", "spring", index * 0.1, 0.5)}
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className="green-pink-gradient shadow-card w-full rounded-[20px] p-[1px]"
       >
         <div className="bg-tertiary flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] px-12 py-5">
@@ -52,10 +52,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="text-secondary mt-4 max-w-3xl text-[17px] leading-[30px]"
       >
-      {/* */}  {config.sections.about.content}
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10 max-sm:justify-center">
+      <div className="mt-20 flex flex-wrap gap-10 justify-center">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
